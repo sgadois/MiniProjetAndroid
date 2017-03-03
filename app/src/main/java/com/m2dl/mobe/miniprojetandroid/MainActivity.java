d3de087169ccb3249e20a6fe076fcd41ee337df0
mingli974 <mumuzgirl@hotmail.com>
2017-03-03 11:48:04 +0100

    Interface de l'activité Main
    Interface de lancement de l'appli

--- app/src/main/java/com/m2dl/mobe/miniprojetandroid/MainActivity.java
+++ app/src/main/java/com/m2dl/mobe/miniprojetandroid/MainActivity.java
@@ -1,13 +1,56 @@
 package com.m2dl.mobe.miniprojetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.m2dl.mobe.miniprojetandroid.emploidutemps.ScheduleActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialisation des activités à appeler
+        final Intent schedule = new Intent(this, ScheduleActivity.class);
+
+        Button mafac = (Button) findViewById(R.id.mafac);
+        mafac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO fac intent
+            }
+        });
+        Button mescours = (Button) findViewById(R.id.mescours);
+        mescours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(schedule);
+            }
+        });
+        Button config = (Button) findViewById(R.id.config);
+        config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //TODO config intent
+            }
+        });
+
+
     }
 }
