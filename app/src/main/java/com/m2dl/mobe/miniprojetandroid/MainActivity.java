e965d830f33e741eed50cf7ebd1afb274615f496
Sebastien Gadois <sgadois@gmail.com>
2017-03-15 21:44:25 +0100

    Ajout d'un menu à gauche

--- app/src/main/java/com/m2dl/mobe/miniprojetandroid/MainActivity.java
+++ app/src/main/java/com/m2dl/mobe/miniprojetandroid/MainActivity.java
@@ -3,9 +3,12 @@ package com.m2dl.mobe.miniprojetandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.m2dl.mobe.miniprojetandroid.emploidutemps.ScheduleActivity;
@@ -14,24 +17,33 @@ import com.m2dl.mobe.miniprojetandroid.occupationru.OccupationActivity;
 import com.m2dl.mobe.miniprojetandroid.photo.PhotosActivity;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
+    public void finish() {
+        super.finish();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.geoloc:
                 startActivity(new Intent(this, GeolocalisationActivity.class));
@@ -49,12 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 //TODO
                 return true;
             default:
-            return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected(item);
         }
-    }
-
-    @Override
-    public void finish() {
-        super.finish();
     }
 }
